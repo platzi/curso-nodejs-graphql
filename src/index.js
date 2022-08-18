16ffc2749892f76f2299f89a0b42958709addf4a
Nicolas Molina Monroy <hi@nicobytes.com>
2022-08-18 06:05:48 -0400

    :construction: config cors

--- src/index.js
+++ src/index.js
@@ -9,18 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-const whitelist = ['http://localhost:8080', 'https://myapp.co'];
-const options = {
-  origin: (origin, callback) => {
-    if (whitelist.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('no permitido'));
-    }
-  }
-}
-app.use(cors(options));
+app.use(cors());
 
 require('./utils/auth');
 
