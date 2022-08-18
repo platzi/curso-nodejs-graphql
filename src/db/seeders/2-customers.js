1f7f5dfc28f10e570d71a96c65f4af6b5f5e602d
Nicolas Molina Monroy <hi@nicobytes.com>
2022-08-18 17:06:18 -0400

    :construction: fix seed data

--- src/db/seeders/2-customers.js
+++ src/db/seeders/2-customers.js
@@ -10,7 +10,7 @@ module.exports = {
         name: 'Juanita',
         last_name: 'Perez',
         phone: '7830601',
-        user_id: 1,
+        user_id: 2,
         created_at: new Date()
       },
     ]);
