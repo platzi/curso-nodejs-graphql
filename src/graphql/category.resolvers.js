655a84bea96a94839a6c18de19fd48e02a4e68d3
Nicolas Molina Monroy <hi@nicobytes.com>
2022-08-19 18:11:16 -0400

    :construction: jwt

--- src/graphql/category.resolvers.js
+++ src/graphql/category.resolvers.js
@@ -1,7 +1,12 @@
+const boom = require('@hapi/boom');
 const CategoryService = require('./../services/category.service');
 const service = new CategoryService();
 
-const addCategory = (_, { dto }) => {
+const addCategory = async (_, { dto }, context) => {
+  const { user } = await context.authenticate('jwt', {session: false});
+  if (!user) {
+    throw boom.unauthorized('jwt is not valid');
+  }
   return service.create(dto);
 }
 
